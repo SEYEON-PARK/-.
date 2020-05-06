f82270eebc3c18cb3d3b7ca172ccd94cb8e5d343
SEYEON_PARK <fourleaf123@naver.com>
2020-05-06 18:51:49 +0900

    파이썬 2주차 공부! 과제 2-2번!

---
+++ PythonApplication3.py
@@ -0,0 +1,10 @@
+#과제 2-2 naMe를 소문자로 바꾸고 뒤에 있는 my를 My로 바꿔라.
+
+a = 'My  naMe  is  Son  Chang  Ha:  my  pin  is  000125-3!!!!!!.'
+a1 = 'naMe'
+a2 = "m"
+
+a1 = a1.lower()
+a2 = a2.upper()
+
+print("My  ", a1, "  is  Son  Chang  Ha:  "+a2+"y  pin  is  000125-3!!!!!!.")
